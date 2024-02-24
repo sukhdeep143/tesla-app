c94c8f8b2f199d84bb99a7c32101e6d46745b97b
Sukhdeep singh <sukhdeepsingh141414@gmail.com>
2024-02-24 20:19:14 +0530

    tesla real link

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,19 +6,19 @@ import MenuIcon from '@mui/icons-material/Menu';
 function Header() {
   return (
     <Container>
-      <a href='#' >
+      <a href='https://www.tesla.com/modely' >
         <img src='/images/logo.svg' alt='Tesla logo' />
       </a>
       <Menu>
-         <a href='#'>Model S</a>
-         <a href='#'>Model 3</a>
-         <a href='#'>Model X</a>
-         <a href='#'>Model Y</a>
+         <a href='https://www.tesla.com/models'>Model S</a>
+         <a href='https://www.tesla.com/model3'>Model 3</a>
+         <a href='https://www.tesla.com/modelx'>Model X</a>
+         <a href='https://tesla.com/modely'>Model Y</a>
 
       </Menu>
       <RightMenu>
-        <a href='#'>Shop</a>
-        <a href='#'>Tesla Account</a>
+        <a href='https://shop.tesla.com/'>Shop</a>
+        <a href='https://auth.tesla.com/en_us/oauth2/v1/authorize?redirect_uri=https%3A%2F%2Fwww.tesla.com%2Fteslaaccount%2Fowner-xp%2Fauth%2Fcallback&response_type=code&client_id=ownership&scope=offline_access%20openid%20ou_code%20email%20phone&audience=https%3A%2F%2Fownership.tesla.com%2F&locale=en-US'>Tesla Account</a>
         <CustomMenu />
 
       </RightMenu>
